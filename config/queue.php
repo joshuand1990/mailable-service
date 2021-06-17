8e7ac8c04ce916dc1e081b7d4de8a74ba3fc0c8a
Joshua DSouza <joshua.dsouza@ae.sacoor.com>
2021-06-17 12:21:18 +0400

    queues to save uuid if failed

--- config/queue.php
+++ config/queue.php
@@ -84,6 +84,7 @@ return [
     'failed' => [
         'database' => env('DB_CONNECTION', 'mysql'),
         'table' => env('QUEUE_FAILED_TABLE', 'failed_jobs'),
+        'driver' => 'database-uuids',
     ],
 
 ];
